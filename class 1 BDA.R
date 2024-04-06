51e9eb16fb48ec93d4d0365de63b0091ed387a8b
shivani9346 <166187572+shivani9346@users.noreply.github.com>
2024-04-05 21:23:50 -0400

    Add files via upload

---
+++ class 1 BDA.R
@@ -0,0 +1,74 @@
+# printing Hello World
+"Hello, World"
+#printing sum of operations
+A <-5
+B <-10
+c <-A + B
+c
+#printing Quotient of operation
+v1 <- 4.5
+v2 <- 2
+v3 <- v1 / v2
+v3
+v4 <- sqrt(v1)
+v4
+#printing more variables
+greet <-"Hello,"
+name <-"Shivani"
+message <- paste(greet,name)
+message
+#printing type data
+x <- 2L
+typeof(x)
+y <- 2.5
+typeof(y)
+z <- 2+3i
+typeof(z)
+a <- "h"
+typeof(a)
+q <- T
+typeof(q)
+p <- FLASE
+p <- FALSE
+typeof(p)
+#printing basic Math
+a <- 5
+b <- 3
+c <- a + b
+d <- a - b
+e <- a * B
+f <- a / b
+
+a
+b
+c
+d
+e
+f
+#printing basic logic
+4 <5
+10> 100
+4==5
+
+result <- 4 < 5
+result
+typeof(result)
+#printing the data
+result <- 4 < 5
+result
+typeof(result)
+
+result2 <- !(5>1)
+result2
+
+result | result2
+result & result2
+
+isTRUE(result)
+
+#printing average of 3 numbers
+s <- 7
+h <- 12
+i <- 5
+average <- (s + h + i) / 3
+print(paste("average"))
